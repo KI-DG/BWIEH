6b32b8a7e4af50bc362c4c6f3dc0e1e691bf3d92
LEE KI YONG <109258291+KI-DG@users.noreply.github.com>
2023-12-26 15:50:41 +0900

    11055_가장_큰_증가하는_부분_수열_문제해결

---
+++ 11055_가장_큰_증가하는_부분_수열/s1.py
@@ -0,0 +1,21 @@
+"""
+10
+1 100 2 50 60 3 5 6 7 8
+
+113
+"""
+
+n = int(input())
+
+arr = list(map(int, input().split()))
+arr.insert(0, 0)
+dp = [0] * (n + 1)
+
+for i in range(1, n + 1):
+    for j in range(i):
+        if arr[i] > arr[j]:
+            dp[i] = max(dp[i] , dp[j] + arr[i])
+        else:
+            dp[i] = max(dp[i], arr[i])
+
+print(max(dp))
