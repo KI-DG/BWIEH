0046e8a886009bc41136ef64c13dc6cb9d820681
LEE KI YONG <109258291+KI-DG@users.noreply.github.com>
2023-12-29 16:21:42 +0900

    [FEAT] 1699_제곱수의_합_문제해결

---
+++ 1699_제곱수의 합/s1.py
@@ -0,0 +1,24 @@
+"""
+7
+
+4
+
+1
+
+1
+
+4
+
+1
+"""
+
+n = int(input())
+dp = [x for x in range(n + 1)]
+
+for i in range(1, n + 1):
+    for j in range(i):
+        if j * j > i:
+            break
+        if dp[i] > dp[i - j * j] + 1:
+            dp[i] = dp[i - j * j] + 1
+print(dp[n])
