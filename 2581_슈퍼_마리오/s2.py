d026b3e23eea5dd76694c54669466d8569bc288c
LEE KI YONG <109258291+KI-DG@users.noreply.github.com>
2024-04-01 19:33:28 +0900

    [FEAT] 2167_2차원_배열의_합_문제해결

---
+++ 2581_슈퍼_마리오/s2.py
@@ -0,0 +1,50 @@
+"""
+10
+20
+30
+40
+50
+60
+70
+80
+90
+100
+
+100
+
+1
+2
+3
+5
+8
+13
+21
+34
+55
+89
+
+87
+
+40
+40
+40
+40
+40
+40
+40
+40
+40
+40
+
+120
+"""
+arr = [int(input()) for _ in range(10)]
+answer = 0
+for i in arr:
+    answer += i
+    if answer >= 100:
+        if answer - 100 > 100 - (answer - i):
+            answer -= i
+            break
+
+print(answer)
