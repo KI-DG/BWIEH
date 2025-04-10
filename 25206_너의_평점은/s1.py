6bb654329336ef9119c85801563b1a15371b6b8c
LEE KI YONG <109258291+KI-DG@users.noreply.github.com>
2025-04-10 15:39:19 +0900

    [FEAT] 25206_너의_평점은_문제해결

---
+++ 25206_너의_평점은/s1.py
@@ -0,0 +1,36 @@
+"""
+ObjectOrientedProgramming1 3.0 A+
+IntroductiontoComputerEngineering 3.0 A+
+ObjectOrientedProgramming2 3.0 A0
+CreativeComputerEngineeringDesign 3.0 A+
+AssemblyLanguage 3.0 A+
+InternetProgramming 3.0 B0
+ApplicationProgramminginJava 3.0 A0
+SystemProgramming 3.0 B0
+OperatingSystem 3.0 B0
+WirelessCommunicationsandNetworking 3.0 C+
+LogicCircuits 3.0 B0
+DataStructure 4.0 A+
+MicroprocessorApplication 3.0 B+
+EmbeddedSoftware 3.0 C0
+ComputerSecurity 3.0 D+
+Database 3.0 C+
+Algorithm 3.0 B0
+CapstoneDesigninCSE 3.0 B+
+CompilerDesign 3.0 D0
+ProblemSolving 4.0 P
+
+3.284483
+
+"""
+sum_score = 0
+my_score = 0
+grade_avg = {'A+': 4.5, 'A0': 4.0, 'B+': 3.5, 'B0': 3.0, 'C+': 2.5, 'C0': 2.0, 'D+': 1.5, 'D0': 1.0, 'F': 0.0}
+for i in range(20):
+    subjects, score, grade = list(map(str, input().split()))
+    score = float(score)
+    if grade != 'P':
+        my_score += score * grade_avg[grade]
+        sum_score += score
+
+print(round(my_score / sum_score, 6))
